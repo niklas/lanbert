5853530df58888100c9dfb8a0d4355b055c41fd8
Niklas Hofer <niklas+dev@lanpartei.de>
2011-11-28 14:45:52 +0100

    move configuration loading into Bot class

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,10 @@
 require 'rubygems'
 require 'bundler/setup'
 
-require 'ostruct'
-parsed_yaml = YAML.parse_file(File.expand_path('../../config.yml', __FILE__)).to_ruby
-config = OpenStruct.new parsed_yaml
+$:.unshift File.expand_path('../../lib', __FILE__)
+require 'bot'
 
-localhost = `hostname`.chomp
-debug = localhost != config.host
-
-if debug
+if Bot.debug?
   Bundler.require(:default, :development)
 else
   Bundler.require(:default)
@@ -17,7 +13,5 @@ end
 require 'active_support/core_ext/class/attribute'
 require 'active_support/core_ext/string/conversions'
 
-$:.unshift File.expand_path('../../lib', __FILE__)
 require 'responsive'
-require 'bot'
 
--- lib/bot.rb
+++ lib/bot.rb
@@ -1,6 +1,25 @@
 require 'pathname'
+require 'ostruct'
 class Bot
-  class_attribute :root
-  self.root = Pathname.new( File.dirname(__FILE__) ).join('..')
+  def self.root
+    @@root ||= Pathname.new( File.dirname(__FILE__) ).join('..')
+  end
+
+  def self.config
+    @config ||= load_config
+  end
+
+  def self.load_config
+    parsed_yaml = YAML.parse_file(File.expand_path('../../config.yml', __FILE__)).to_ruby
+    OpenStruct.new parsed_yaml
+  end
+
+  def self.debug?
+    localhost != config.host
+  end
+
+  def self.localhost
+    @localhost ||= `hostname`.chomp
+  end
 end
 
